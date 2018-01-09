f49442647fa3aaf60218fba87bf7e6aeea1c193c
Joshua Jones <kilauea.jones@gmail.com>
2018-01-09 00:13:23 -0600

    Setup infrastructure for a very basic bot

---
+++ StonehengeBot/Bot.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace StonehengeBot
+{
+    public class Bot
+    {
+        private readonly IConfigurationRoot _configuration;
+        private readonly ILogger<Bot> _logger;
+
+        public Bot(IConfigurationRoot configuration, ILogger<Bot> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task Run()
+        {
+            using (var client = new DiscordSocketClient())
+            {
+                client.Log += Log;
+                client.MessageReceived += MessageReceived;
+
+                await client.LoginAsync(TokenType.Bot, _configuration["Token"]);
+                await client.StartAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+        private Task Log(LogMessage message)
+        {
+            LogLevel logLevel;
+            switch (message.Severity)
+            {
+                case LogSeverity.Critical:
+                    logLevel = LogLevel.Critical;
+                    break;
+                case LogSeverity.Error:
+                    logLevel = LogLevel.Error;
+                    break;
+                case LogSeverity.Warning:
+                    logLevel = LogLevel.Warning;
+                    break;
+                case LogSeverity.Info:
+                    logLevel = LogLevel.Information;
+                    break;
+                case LogSeverity.Verbose:
+                    logLevel = LogLevel.Trace;
+                    break;
+                case LogSeverity.Debug:
+                    logLevel = LogLevel.Debug;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            _logger.Log(logLevel, 0, message, message.Exception, (logMessage, exception) => logMessage.ToString(null, prependTimestamp: false));
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- StonehengeBot/Program.cs
+++ StonehengeBot/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace StonehengeBot
 {
     internal static class Program
     {
+        private const string EnvironmentPrefix = "STONEHENGEBOT_";
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Load configuration
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables(EnvironmentPrefix)
+                .AddCommandLine(args)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    builder.AddConfiguration(configuration);
+                    builder.AddConsole();
+                })
+                .AddSingleton(provider => new Bot(configuration, provider.GetRequiredService<ILogger<Bot>>()))
+                .BuildServiceProvider(true);
+
+            var bot = serviceProvider.GetRequiredService<Bot>();
+            bot.Run().GetAwaiter().GetResult();
         }
     }
 }
