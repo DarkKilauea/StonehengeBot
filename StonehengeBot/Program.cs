36b41b196ae2d6c174489885d11e1bedadf00dec
Joshua Jones <kilauea.jones@gmail.com>
2018-01-08 22:13:24 -0600

    Add project files.

---
+++ StonehengeBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StonehengeBot
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
